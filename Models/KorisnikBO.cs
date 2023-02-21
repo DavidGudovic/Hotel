d0a0709ce192f988d2b4e8c428192ed6c57e1085
David <dgudovic1@gmail.com>
2023-02-21 19:31:10 +0100

    Added validation

--- Models/KorisnikBO.cs
+++ Models/KorisnikBO.cs
@@ -1,12 +1,21 @@
-﻿namespace Hotel.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
 {
     public class KorisnikBO
     {
         public int KorisnikID { get; set; }
+        [Required(ErrorMessage ="Morate uneti korisničko ime")]
         public string KorisnickoIme { get; set; }
+        [Required(ErrorMessage = "Morate uneti šifru")]
+        [MinLength(8,ErrorMessage = "Šifra mora biti duža od 8 karaktera")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Morate uneti ime")]
         public string Ime { get; set; }
+        [Required(ErrorMessage = "Morate uneti Prezime")]
         public string Prezime { get; set; }
+        [Required(ErrorMessage = "Morate uneti email")]
+        [EmailAddress(ErrorMessage = "Invalidna email adresa")]
         public string Email { get; set; }
         public Role Rola { get; set; }
     }
