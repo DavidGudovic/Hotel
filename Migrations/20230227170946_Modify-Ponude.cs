12bb1b26484072ead6370da47a3625a4018f3526
David <dgudovic1@gmail.com>
2023-02-27 21:03:44 +0100

    database update

---
+++ Migrations/20230227170946_Modify-Ponude.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Migrations
+{
+    public partial class ModifyPonude : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Opis",
+                table: "Ponude",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Opis",
+                table: "Ponude");
+        }
+    }
+}
