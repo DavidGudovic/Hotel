f930a153e7af6e1fe237d86aa3a209ece24c509b
David <dgudovic1@gmail.com>
2023-02-28 16:30:11 +0100

    Started implementing booking functionality

--- Controllers/RezervacijaController.cs
+++ Controllers/RezervacijaController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel.Controllers
 {
     public class RezervacijaController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(RezervacijaBO rezervacija)
         {
             return View();
         }
