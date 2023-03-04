1f4eb00b7ed814f0ed423fb430680f17d3b62e75
David <dgudovic1@gmail.com>
2023-03-04 17:29:37 +0100

    Implemented file upload service

---
+++ Models/Services/ImageService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace Hotel.Models.Services
+{
+    public class ImageService
+    {
+        public async Task<string> UploadAsync(IFormFile image_file, IWebHostEnvironment webHostEnvironment)
+        {
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(image_file.FileName);
+            // wwwroot/images/apartments/FileName.jpg
+            string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images/apartments", $"{fileNameWithoutExtension}.jpg");
+            // copy the uploaded file to the wwwroot/images folder
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await image_file.CopyToAsync(fileStream);
+            }
+            return fileNameWithoutExtension;
+        }
+    }
+}
